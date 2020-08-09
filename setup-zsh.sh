966d44d6742f320a82fe150288ba6c6375398697
Konrad Odell <konradish@gmail.com>
2020-08-09 10:10:36 -0500

    Setup

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
-curl -Lo ~/convert_bash_to_zsh.sh https://gist.github.com/muendelezaji/c14722ab66b505a49861b8a74e52b274/raw/49f0fb7f661bdf794742257f58950d209dd6cb62/bash-to-zsh-hist.py
-chmod +x ~/bash-to-zsh-hist.py
-cat ~/.bash_history | bash-to-zsh-hist.py >> ~/.zsh_history
-#curl -L git.io/antigen > ~/.oh-my-zsh/antigen.zsh
+if [[ ! -f "~/.zshrc" ]]
+then
+    echo "Installing zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+    curl -Lo ~/convert_bash_to_zsh.sh https://gist.github.com/muendelezaji/c14722ab66b505a49861b8a74e52b274/raw/49f0fb7f661bdf794742257f58950d209dd6cb62/bash-to-zsh-hist.py
+    chmod +x ~/bash-to-zsh-hist.py
+    echo "Converting bash history"
+    cat ~/.bash_history | bash-to-zsh-hist.py >> ~/.zsh_history
+    source ~/.zshrc
+fi
 
-source ~/.zshrc
+if [[ ! -f "~/.oh-my-zsh/antigen.zsh" ]]
+then
+    curl -L git.io/antigen > ~/.oh-my-zsh/antigen.zsh
+    source ~/.oh-my-zsh/antigen.zsh
+fi
+
