aad76e10fa8c6bee2ea95778fc9a3f3cabdd856e
Konrad Odell <konradish@gmail.com>
2020-10-18 10:52:11 -0500

    Works with CentOS

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,4 @@
 docker build . -t dotfiles_test_container && \
-docker run -e LANG=C.UTF-8 -e LC_ALL=C.UTF-8 -e TERM=$TERM --rm -v $(pwd):/root/dotfiles -w /root/dotfiles dotfiles_test_container ./setup-env.sh
+#docker run -it -e LANG=C.UTF-8 -e LC_ALL=C.UTF-8 -e TERM=$TERM --rm -v $(pwd):/root/dotfiles -w /root/dotfiles dotfiles_test_container ./setup-env.sh
+
+docker run -it -e LANG=C.UTF-8 -e LC_ALL=C.UTF-8 -e TERM=$TERM --rm -v $(pwd):/root/dotfiles -w /root/dotfiles centos ./setup-env.sh
--- setup-env.sh
+++ setup-env.sh
@@ -6,8 +6,11 @@ if [ "$EUID" -ne 0 ]
 then
   SUDO=sudo
 fi
-echo "Installing dependencies"
-$SUDO apt-get update && $SUDO apt-get install -y zsh git curl autojump
+
+echo "Installing zsh cURL and git dependencies"
+$SUDO ./pacapt install --noconfirm -S zsh git curl
+#echo "Installing autojump"
+#$SUDO ./pacapt install --noconfirm -S autojump-zsh || echo "Autojump not found"
 
 convert_bash_hist() {
   curl -Lo ~/bash-to-zsh-hist.py https://gist.github.com/muendelezaji/c14722ab66b505a49861b8a74e52b274/raw/49f0fb7f661bdf794742257f58950d209dd6cb62/bash-to-zsh-hist.py
