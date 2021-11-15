2be35939868c53b31970a2cbd6b643ac6abc689e
Konrad Odell <konradish@gmail.com>
2021-11-14 18:31:38 -0600

    Overhaul

--- setup-env.sh
+++ setup-env.sh
@@ -6,24 +6,22 @@ if [ "$EUID" -ne 0 ]
 then
   SUDO=sudo
 fi
+source scripts/make_gpg.sh
+
 echo "Updating software packages"
 $SUDO ./pacapt -Sy
 echo "Installing zsh cURL and git dependencies"
-$SUDO ./pacapt install --noconfirm -S zsh git curl
-#echo "Installing autojump"
+$SUDO ./pacapt install --noconfirm -S zsh git curl stow rclone gpg
 
-convert_bash_hist() {
-  curl -Lo ~/bash-to-zsh-hist.py https://gist.github.com/muendelezaji/c14722ab66b505a49861b8a74e52b274/raw/49f0fb7f661bdf794742257f58950d209dd6cb62/bash-to-zsh-hist.py
-  chmod +x ~/bash-to-zsh-hist.py
-  echo "Converting bash history"
-  cat ~/.bash_history | bash-to-zsh-hist.py >> ~/.zsh_history
+unstow() {
+	stow -t ~ stow/*
+	stow -t ~ secrets/*
 }
 
 install_plugins() {
   git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
   git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
-  #curl -Lo $HOME/Spaceship10kTheme http://bit.ly/Spaceship10kTheme
   echo "source ~/dotfiles/Spaceship10kTheme" >~/.zshrc
 }
 
@@ -32,8 +30,8 @@ if [ ! -f "$HOME/.zshrc" ]
 then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --skip-chsh
 fi
-
+#$SUDO rclone configure
+#restore_secrets
 install_plugins
-
-grep 'alias config' $HOME/.zshrc || echo "alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'" >> ~/.zshrc
+#unstow
 exec zsh
